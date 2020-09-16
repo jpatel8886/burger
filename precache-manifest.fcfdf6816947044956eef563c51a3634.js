aba9cbd145ca5074dd00c3756e3ce8662c0e3d89
jpatel8886 <jpatel34@wisc.edu>
2020-09-16 19:02:47 +0530

    Updates

---
+++ precache-manifest.fcfdf6816947044956eef563c51a3634.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff44bd1e17920bcdbcf47d44eadc9f1d",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "0d193835f697f31895e5",
+    "url": "/burger/static/css/main.0b03613d.chunk.css"
+  },
+  {
+    "revision": "1869b22a2610dc0a52a2",
+    "url": "/burger/static/js/2.4723876a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger/static/js/2.4723876a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d193835f697f31895e5",
+    "url": "/burger/static/js/main.a1c5e704.chunk.js"
+  },
+  {
+    "revision": "b70cca1f39cb2c1dd795",
+    "url": "/burger/static/js/runtime-main.95ddc443.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
